0b02d479369f75237757dff42100fc7d00e04a84
ukokpala <okpalaukamaka480@gmail.com>
2023-09-04 12:28:56 -0700

    added tasks

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnet);
 router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', AuthController.getMe);
 router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
