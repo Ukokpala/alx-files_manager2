6c62d0eea8926654ece9f048f6cda9229a83077f
Ukamaka Okpala <111020223+Ukokpala@users.noreply.github.com>
2023-09-06 02:17:51 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-
 #!/usr/bin/node
 
 const express = require('express');
@@ -6,17 +5,13 @@ const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
 
-
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
 router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
-
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', AuthController.getMe);
 
-
-export default router;
+module.exports = router;
