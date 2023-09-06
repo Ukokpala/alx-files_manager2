3a37715db8d3448762a4ed0414ad391ff360d553
Ukamaka Okpala <111020223+Ukokpala@users.noreply.github.com>
2023-09-06 02:10:50 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
-const FilesController = require('../controllers/FilesController');
+
 
 const router = express.Router();
 
@@ -18,11 +18,5 @@ router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/publish', FilesController.putUnpublish);
-router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
